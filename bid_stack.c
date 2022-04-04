27999ea3d3ae93bd0bbe012a763be9b9ca205272
noeliaserrano <noelia.serrano@udc.es>
2022-04-04 13:42:36 +0200

    tad stack

--- bid_stack.c
+++ bid_stack.c
@@ -9,4 +9,24 @@
 
 #include "bid_stack.h"
 
-/* Write your code here... */
+void createEmptyStack(tStack *stack){
+    stack -> top = SNULL;
+}
+bool push(tItemS d, tStack *stack){
+    if(stack -> top == SMAX -1) //pila llena
+        return false;
+    else{
+        stack -> top++;
+        stack ->data[stack->top]=d;
+        return true;
+    }
+}
+void pop(tStack *stack){
+    stack -> top--;
+}
+tItemS peek(tStack stack){
+    return stack.data[stack.top];
+}
+bool isEmptyStack(tStack stack){
+    return (stack.top == SNULL);
+}
--- bid_stack.h
+++ bid_stack.h
@@ -4,14 +4,38 @@
  * AUTHOR 1: ***************************** LOGIN 1: **********
  * AUTHOR 2: ***************************** LOGIN 2: **********
  * GROUP: *.*
- * DATE: ** / ** / **
+ * DATE: 08 / 04 / 22
  */
 
 #ifndef BID_STACK_H
 #define BID_STACK_H
 
 #include "types.h"
+#include <stdbool.h>
 
-/* Write your code here... */
+#define SNULL -1
+#define SMAX 25
+
+typedef int tPosS;
+
+typedef struct tItemS {
+    tUserId bidder;
+    tProductPrice productPrice;
+} tItemS;
+
+typedef struct stack{
+    tItemS data[SMAX];
+    int top;
+}tStack;
+
+void createEmptyStack(tStack *stack);
+
+bool push(tItemS d, tStack *stack);
+
+void pop(tStack *stack);
+
+tItemS peek(tStack stack);
+
+bool isEmptyStack(tStack stack);
 
 #endif
